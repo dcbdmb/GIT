071bfe7f157f4f597d4e5f6e565d75ffdcfddc9b
dcbdmb <dcbdmb@gmail.com>
2018-01-21 21:58:12 -0500

    updating code

--- creategit.sh
+++ creategit.sh
@@ -14,7 +14,6 @@ do
   esac
 done
 
-# Curl some json to the github API oh damn we so fancy
 curl -u ${GITHUBUSER} https://api.github.com/user/repos -d "{\"name\": \"${REPONAME}\", \"description\": \"${DESCRIPTION}\", \"private\": false, \"has_issues\": true, \"has_downloads\": true, \"has_wiki\": false}"
 
 git remote add origin https://github.com/${GITHUBUSER}/${REPONAME}
