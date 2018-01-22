9f1c844c04ef31b310a3c84f344bef775fc7aa85
dcbdmb <dcbdmb@gmail.com>
2018-01-21 21:44:42 -0500

    update creatgit.sh to take in alternate reponame

--- creategit.sh
+++ creategit.sh
@@ -4,6 +4,15 @@ REPONAME=${PWD##*/}
 # Get user input
 read -p "Git Username:" GITHUBUSER
 read -p "Repo Description:" DESCRIPTION
+read -p "Override current directory name?::" answer
+while true
+do
+  case $answer in
+   [yY]* ) read -p "Enter new RepoName:" REPONAME
+           break;;
+   [nN]* ) exit;;
+  esac
+done
 
 # Curl some json to the github API oh damn we so fancy
 curl -u ${GITHUBUSER} https://api.github.com/user/repos -d "{\"name\": \"${REPONAME}\", \"description\": \"${DESCRIPTION}\", \"private\": false, \"has_issues\": true, \"has_downloads\": true, \"has_wiki\": false}"
